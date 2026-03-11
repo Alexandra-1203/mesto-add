59ebda52172a48a65ff6a6d79a12141a4b8090f4
Alexandra Feofanova <feofanovaaleksandra1@gmail.com>
2026-03-11 23:21:43 +0300

    fh

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  server: {
-    open: true,
-  },
-  base: "./",
-});
-=======
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -16,4 +6,3 @@ export default defineConfig({
   },
   base: './',
 });
->>>>>>> bcc222a851f366857510b1a1e7fab25c4a6047b4
